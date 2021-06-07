c3f5d37c4ad823d364e0c47d4b35f2806f0dac39
Brian Cabacungan <lbcab@uw.edu>
2021-06-07 00:15:02 -0700

    rev1.2

--- library-app/src/App.js
+++ library-app/src/App.js
@@ -2,12 +2,61 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Mainpage from './Mainpage';
+import Bestsellers from './Bestsellers';
+import Bookshelf from './Bookshelf';
+import Cart from './Cart';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Mainpage />
+
+        <button class="button">Home
+          <Router>
+            <Switch>
+              <Route exact path="/Main"/>
+            </Switch>
+          </Router>
+        </button>
+
+        <br/>
+        <br/>
+        <br/>
+
+        <button class="button">Bestsellers
+          <Router>
+            <Switch>
+              <Route path="/Bestsellers" component={Bestsellers}/>
+            </Switch>
+          </Router>
+        </button>
+
+        <br/>
+        <br/>
+        <br/>
+
+        <button class="button">Bookshelf
+          <Router>
+            <Switch>
+              <Route exact path="/" />
+            </Switch>
+          </Router>
+        </button>
+
+        <br/>
+        <br/>
+        <br/>
+
+        <button class="button">Cart
+          <Router>
+            <Switch>
+              <Route exact path="/" />
+            </Switch>
+          </Router>
+        </button>
+
       </div>
     )
   }
--- library-app/src/Bestsellers.js
+++ library-app/src/Bestsellers.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class Bestsellers extends React.Component {
-    
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1>Bestsellers</h1>
+                </div>
+
+            <div>
+                <button type="button">
+                    <Link to="/Mainpage"></Link>
+                </button>
+            </div>
+                
+            </div>
+        )
+    }
 }
 
 export default Bestsellers;
